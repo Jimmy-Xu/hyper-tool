dcddf28b31e29fcc0f22b98a910a351ef219977c
Jimmy Xu <xjimmyshcn@gmail.com>
2015-05-22 13:29:09 +0800

    [master] modify memory usage test

--- cmd/test/view-memory-usage.sh
+++ cmd/test/view-memory-usage.sh
@@ -11,18 +11,18 @@ show_message "view memory usage" green
 NUM_QEMU_PROC=$(ps aux | sed -n '1p;/'${QEMU_PROCESS}'/p' | grep -v sed| grep qemu | wc -l)
 
 show_message "QEMU process memory usage(KiB): ( ${PURPLE}${NUM_QEMU_PROC}${GREEN} qemu process )" green bold
-echo "================================"
-echo -e "     min\tmax\tavg"
-echo "--------------------------------"
+echo "==========================================="
+echo -e "               min        max     avg"
+echo "==========================================="
 STAT_RLT_RSS=$(ps aux | sed -n '1p;/'${QEMU_PROCESS}'/p' | grep -v sed | awk '{print $2,$5,$6,$11}'| grep qemu | cut -d" " -f3 \
-| awk '{if(min==""){min=max=$1}; if($1>max) {max=$1}; if($1< min) {min=$1}; total+=$1; count+=1} END { if (count>0){ printf "%s\t%s\t%s",min,max,total/count}else{print ""}; }')
-echo "RSS: ${PURPLE}${STAT_RLT_RSS}${RESET}"
-echo "================================"
+| awk '{if(min==""){min=max=$1}; if($1>max) {max=$1}; if($1< min) {min=$1}; total+=$1; count+=1} END { if (count>0){ printf "%5.0f\t%5.0f\t%5.0f",min/1024,max/1024,total/count/1024}else{print ""}; }')
+echo "RSS(VmRSS) : ${PURPLE}${STAT_RLT_RSS}${RESET}"
+echo "-------------------------------------------"
 
 STAT_RLT_VSZ=$(ps aux | sed -n '1p;/'${QEMU_PROCESS}'/p' | grep -v sed | awk '{print $2,$5,$6,$11}'| grep qemu | cut -d" " -f2 \
-| awk '{if(min==""){min=max=$1}; if($1>max) {max=$1}; if($1< min) {min=$1}; total+=$1; count+=1} END { if (count>0){ printf "%s\t%s\t%s",min,max,total/count}else{print ""}; }')
-echo "VSZ: ${PURPLE}${STAT_RLT_VSZ}${RESET}"
-echo "================================"
+| awk '{if(min==""){min=max=$1}; if($1>max) {max=$1}; if($1< min) {min=$1}; total+=$1; count+=1} END { if (count>0){ printf "%5.0f\t%5.0f\t%5.0f",min/1024,max/1024,total/count/1024}else{print ""}; }')
+echo "VSZ(VmSize): ${PURPLE}${STAT_RLT_VSZ}${RESET}"
+echo "==========================================="
 
 
 show_message "Done." green
