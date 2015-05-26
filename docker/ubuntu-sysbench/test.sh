df2b667d851e65f5fc68d44b6af6e3cac6336f4c
Jimmy Xu <xjimmyshcn@gmail.com>
2015-05-26 18:17:23 +0800

    [master] modify test.sh

--- docker/ubuntu-sysbench/test.sh
+++ docker/ubuntu-sysbench/test.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
-#~/test.sh
-cd /root/
-rm cpu.log -rf
-touch cpu.log
-echo `date +'%F %T'` 1'(1,5000)';sysbench --test=cpu --cpu-max-prime=5000 --num-threads=1 run | grep 'total time:' >>cpu.log
-echo `date +'%F %T'` 2'(1,10000)';sysbench --test=cpu --cpu-max-prime=10000 --num-threads=1 run | grep 'total time:' >>cpu.log
-echo `date +'%F %T'` 3'(1,50000)';sysbench --test=cpu --cpu-max-prime=50000 --num-threads=1 run | grep 'total time:' >>cpu.log
-echo `date +'%F %T'` 4'(2,5000)';sysbench --test=cpu --cpu-max-prime=5000 --num-threads=2 run | grep 'total time:' >>cpu.log
-echo `date +'%F %T'` 5'(2,10000)';sysbench --test=cpu --cpu-max-prime=10000 --num-threads=2 run | grep 'total time:' >>cpu.log
-echo `date +'%F %T'` 6'(2,50000)';sysbench --test=cpu --cpu-max-prime=50000 --num-threads=2 run | grep 'total time:' >>cpu.log
-echo `date +'%F %T'`' Done'
-echo
-cat cpu.log
+export PATH=/usr/local/bin:${PATH}
 
+NUM_THREADS=(1 2)
+CPU_MAX1=(5000 10000 50000)
+CPU_MAX2=(10000 50000 100000)
+
+
+cd /root/
+if [ $# -eq 0 ] || [ $# -gt 0 -a "$1" == "1" ];then
+  echo "use CPU_MAX1"
+  CPU_MAX=(${CPU_MAX1[@]})
+  LOG=cpu1.log
+else
+  echo "use CPU_MAX2"
+  CPU_MAX=(${CPU_MAX2[@]})
+  LOG=cpu2.log
+fi
+
+\rm ${LOG} -rf
+touch ${LOG}
+
+idx=1
+echo
+echo "$(date +'%F %T') Begin..."
+for (( i = 0 ; i < ${#NUM_THREADS[@]} ; i++ ))
+do
+  thread="${NUM_THREADS[$i]}"
+  for (( j = 0 ; j < ${#CPU_MAX[@]} ; j++ ))
+  do
+    cpu_max="${CPU_MAX[$j]}"
+    echo "$(date +'%F %T') ${idx} (${thread},${cpu_max})";sysbench --test=cpu --cpu-max-prime=${cpu_max} --num-threads=${thread} run | grep 'total time:' >>${LOG}
+    idx=$((idx+1))
+  done
+done
+
+echo
+echo "$(date +'%F %T') End!"
+cat ${LOG}
