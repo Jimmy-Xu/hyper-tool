b0968361fb5a047447f20fba6d257a2efac7a587
Jimmy Xu <xjimmyshcn@gmail.com>
2015-06-03 15:41:51 +0800

    [master] support rpm

--- cmd/hyperinit/install-deps-hyperinit.sh
+++ cmd/hyperinit/install-deps-hyperinit.sh
@@ -10,6 +10,11 @@ is_hyperinit_exist
 
 #main
 cd "$GOPATH/src/${HYPERINIT_CLONE_DIR}"
-sudo apt-get install -y autoconf automake
+which apt-get >/dev/null 2>&1
+if [ $? -eq 0 ];then
+	sudo apt-get install -y autoconf automake
+else
+	sudo yum install -y autoconf automake
+fi
 
 show_message "Done." green
